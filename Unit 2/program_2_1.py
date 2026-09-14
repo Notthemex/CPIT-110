cb7efdca6a290f458f1d0c38717541422bc2369b
NotTheMex <youseftlabani@gmail.com>
2026-09-14 13:31:08 +0300

    fixed a small typo

--- Unit 2/program_2_1.py
+++ Unit 2/program_2_1.py
@@ -5,4 +5,4 @@ pi = 3.14159
 #Compute area using formula Area^2 * radius
 area = radius * radius * pi
 #Print out the area
-print(f"Based off of the radius ({radius}, the area of your circle is: {area})")
+print(f"Based off of the radius ({radius}), the area of your circle is: {area}")
