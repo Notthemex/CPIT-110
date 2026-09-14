b87a9fb261632b425fbb7c5bb832e8deb4cad4f8
NotTheMex <youseftlabani@gmail.com>
2026-09-14 13:30:13 +0300

    changed the naming format so it makes more sense created unit 2 file made teh first program for unit 2 ( program_2_1.py )

---
+++ Unit 2/program_2_1.py
@@ -0,0 +1,8 @@
+#Ask the user for the radius
+radius = int(input("Enter the radius: "))
+#Assign value of pi
+pi = 3.14159
+#Compute area using formula Area^2 * radius
+area = radius * radius * pi
+#Print out the area
+print(f"Based off of the radius ({radius}, the area of your circle is: {area})")
