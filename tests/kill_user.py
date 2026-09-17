559c85b1e18652b56656f43914ddfa569c4564ad
NotTheMex <youseftlabani@gmail.com>
2026-09-17 16:07:03 +0300

    made the code output better

--- tests/kill_user.py
+++ tests/kill_user.py
@@ -2,7 +2,4 @@ names = []
 number_of_names = eval(input("Enter the number of names: "))
 for i in range(number_of_names):
     names.append(input("Enter name: "))
-count = 0
-while count < number_of_names:
-    print(f"Kill {names[count]}")
-    count += 1
+print(f"Kill {", " .join(names)}")
